d04cc6edc61b10237d03c59d1d1c796bd6bebf70
Steve Nolen <technolengy@gmail.com>
2015-08-13 07:31:59 -0700

    add sshd-world, sshd opens only to required internal ips. reject es requests from not accepted host.

---
+++ recipes/sshd-world.rb
@@ -0,0 +1,17 @@
+#
+# Cookbook Name:: ectg-iptables
+# Recipe:: default
+#
+# Copyright (C) 2015 UC Regents
+#
+include_recipe 'ectg-iptables'
+
+iptables_rule 'iptables_sshd-world' do
+  # look for this rule in templates/default/iptables_sshd-world.
+  source 'sshd-world.erb'
+  name 'sshd-world'
+  variables(
+    port: 22 # abstract as variable if we ever need to set not as 22.
+  )
+  action :enable
+end
