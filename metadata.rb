8dcaad45a2f87dac10034b6e2d7b3d390d4fe0d3
Steve Nolen <technolengy@gmail.com>
2016-01-16 16:05:13 -0800

    re-license

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
 name 'ectg-iptables'
 maintainer 'Steve Nolen'
 maintainer_email 'technolengy@gmail.com'
-license 'gplv3'
+license 'apachev2'
 description 'Installs/Configures ectg-iptables'
 long_description 'Installs/Configures ectg-iptables'
-version '0.1.1'
+version '0.1.2'
 
 depends 'iptables'
